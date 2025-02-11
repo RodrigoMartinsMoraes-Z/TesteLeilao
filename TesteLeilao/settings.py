279432cd4e26438e8b22de5c70ea46d33824888d
Rodrigo Martins Moraes <rodrigo_moraes@hotmail.com.br>
2025-02-11 10:30:39 -0300

    enviar e-mail

--- TesteLeilao/settings.py
+++ TesteLeilao/settings.py
@@ -120,3 +120,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
 
 LOGIN_URL = '/login/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.example.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'leilao@example.com'
+EMAIL_HOST_PASSWORD = 'senhadoemail'
+DEFAULT_FROM_EMAIL = 'webmasterleilao@example.com'
--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+
+from core.utils import enviar_notificacao_anexo
 from .models import Leiloeiro, Matricula, Anexo, Estado
 
 admin.site.register(Leiloeiro)
 admin.site.register(Matricula)
 admin.site.register(Anexo)
 admin.site.register(Estado)
+
+@admin.register(Anexo)
+class AnexoAdmin(admin.ModelAdmin):
+    list_display = ('arquivo', 'leiloeiro', 'matricula')
+    search_fields = ('leiloeiro__nome', 'matricula__numero')
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        enviar_notificacao_anexo(obj.leiloeiro, obj)
